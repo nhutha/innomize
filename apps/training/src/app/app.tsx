375863b5cce402a5fff5234636fabb3afdefa2b5
narutocs <nhuthahuu@gmail.com>
2020-06-02 16:42:17 +0700

    init project

---
+++ apps/training/src/app/app.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './app.css';
+
+import { ReactComponent as Logo } from './logo.svg';
+import star from './star.svg';
+
+export const App = () => {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./app.css file.
+   */
+  return (
+    <div className="app">
+      APP STARTED
+    </div>
+  );
+};
+
+export default App;
